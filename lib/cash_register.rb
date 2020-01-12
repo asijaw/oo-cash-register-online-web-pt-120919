9b2c87d16d8f66b2006987d0e3c36dd244945a44
Asija Watson <asija.w@yahoo.com>
2020-01-12 21:51:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,8 @@
 class CashRegister
   attr_accessor :cash_register, :cash_register_with_discount, :total
   
-  def initialize 
+  def initialize(employee_discount = 20)
     @total = 0 
-    @cash_register_with_discount = 20
+    @cash_register_with_discount = employee_discount
   end
 end
