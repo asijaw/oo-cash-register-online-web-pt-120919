125508c17ce36de500a2f12915a3873caba9159a
Asija Watson <asija.w@yahoo.com>
2020-01-12 22:06:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :cash_register, :cash_register_with_discount, :total, :discount, :last_transaction
+  attr_accessor :cash_register, :cash_register_with_discount, :total, :discount, :transaction
   
   def initialize(discount = 20)
     @total = 0 
@@ -7,7 +7,10 @@ class CashRegister
   end
   
   def add_item(name, price, quantity = 1)
-    self.last_transaction = price * quantity
-    self.total = total + (price * quantity)
+    self.transaction = price * quantity
+    self.total = self.total + (price * quantity)
   end
+  
+  def apply_discount
+    self.total = self.total * (self.discount / 100)
 end
